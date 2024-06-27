1c23e2f02b9dc4db82e4f8a9c358e4606df9ba70
Ronaldo89 <71316604+Ronaldo89@users.noreply.github.com>
2024-06-27 21:36:04 +0200

    Removed commented part

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,16 +1,16 @@
-/*Create TABLE EmployeeDemographics
+Create TABLE EmployeeDemographics
 (Emplyoyee int,
 FirstName varchar(50),
 LastName varchar(50),
 Age int, 
 Gender varchar(50)
-)*/
+)
 
-/*Create Table EmployeeSalary
+Create Table EmployeeSalary
 ( EmployeeID int,
 JobTitle varchar(50),
 Salary int
-)*/
+)
 
 
 Insert into EmployeeDemographics VALUES
